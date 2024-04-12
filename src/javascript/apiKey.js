781bfb55ce0cd682ecef5cd73c3481495b152f1c
Hin Yau <99358826+nzxzn@users.noreply.github.com>
2024-04-12 16:44:19 -0700

    Add api and basic handling

---
+++ src/javascript/apiKey.js
@@ -0,0 +1,1 @@
+export const apiKey = 'a4041b9ccd574aa5ac6231619241204';
--- src/javascript/layout.js
+++ src/javascript/layout.js
@@ -1,3 +1,5 @@
+import { apiKey } from './apiKey';
+
 const searchBox = () => {
   const searchBoxElement = document.createElement('input');
   searchBoxElement.type = 'text';
@@ -12,10 +14,32 @@ const submitButton = () => {
   return buttonElement;
 };
 
+const getCurrentWeather = async (city) => {
+  const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+
+  try {
+    const weatherData = await fetch(url);
+    const weatherDataJson = await weatherData.json();
+    console.log(weatherDataJson);
+  } catch (error) {
+    console.error('Error: ', error);
+  }
+};
+
+const handleFormSubmit = (e) => {
+  e.preventDefault();
+  const inputElement = e.target.querySelector('input[type="text"]');
+  const inputtedCity = inputElement.value;
+  inputElement.value = '';
+
+  getCurrentWeather(inputtedCity);
+};
+
 const form = () => {
   const formElement = document.createElement('form');
   formElement.appendChild(searchBox());
   formElement.appendChild(submitButton());
+  formElement.addEventListener('submit', handleFormSubmit);
   return formElement;
 };
 
