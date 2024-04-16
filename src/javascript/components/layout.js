fb2a707f5dbb07d282a11522daa092bb17c191d0
Hin Yau <99358826+nzxzn@users.noreply.github.com>
2024-04-16 15:55:30 -0700

    Enhance error handling

--- src/javascript/components/layout.js
+++ src/javascript/components/layout.js
@@ -1,14 +1,33 @@
 import { searchBox } from './searchBox';
 import { submitButton } from './submitButton';
 import { getCurrentWeather } from '../services/getCurrentWeather';
+import { clearElement } from '../utils/clearElement';
+import { dataTableDiv } from './dataTable';
 
-const handleFormSubmit = (e) => {
+const handleFormSubmit = async (e) => {
   e.preventDefault();
   const inputElement = e.target.querySelector('input[type="text"]');
   const inputtedCity = inputElement.value;
   inputElement.value = '';
 
-  getCurrentWeather(inputtedCity);
+  try {
+    await getCurrentWeather(inputtedCity);
+  } catch (error) {
+    let dataTable = document.querySelector('div[data-data-id="dataTable"]');
+
+    if (dataTable == null || dataTable == undefined) {
+      dataTable = dataTableDiv();
+      document.body.appendChild(dataTable);
+    } else {
+      clearElement(dataTable);
+    }
+
+    const erorrMessageSpan = document.createElement('span');
+    erorrMessageSpan.textContent = error;
+    dataTable.appendChild(erorrMessageSpan);
+
+    console.error(error);
+  }
 };
 
 const form = () => {
--- src/javascript/services/getCurrentWeather.js
+++ src/javascript/services/getCurrentWeather.js
@@ -4,15 +4,13 @@ import { apiKey } from '../utils/apiKey';
 export const getCurrentWeather = async (city) => {
   const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
 
-  try {
-    const weatherData = await fetch(url, { mode: 'cors' });
+  const weatherData = await fetch(url, { mode: 'cors' });
 
-    if (!weatherData.ok) {
-      throw new Error(weatherData.message);
-    }
-    const weatherDataJson = await weatherData.json();
-    printData(weatherDataJson);
-  } catch (error) {
-    console.error('Error: ', error);
+  if (!weatherData.ok) {
+    throw new Error(
+      `Failed to fetch weather data. Status Code: ${weatherData.status}`
+    );
   }
+  const weatherDataJson = await weatherData.json();
+  printData(weatherDataJson);
 };
